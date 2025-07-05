18ce54e2fc0034701b3d69d954621f70e653959b
krisha <krishashilpakar8@gmail.com>
2025-07-05 21:11:52 +0545

    Merging load feature with transaction history

--- wallet/migrations/0001_initial.py
+++ wallet/migrations/0001_initial.py
@@ -11,13 +11,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
    initial = True
 
-    dependencies = [
+dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
-    operations = [
+operations = [
         migrations.CreateModel(
-<<<<<<< HEAD
+
             name='Wallet',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
